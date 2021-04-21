a6534c3baceeeb38650bc7c411c160188b0f07ce
William Olsson <burrken55@gmail.com>
2021-04-21 09:07:24 +0200

    have tinkerd on some stuff soon done

--- CarApp/Form1.Designer.cs
+++ CarApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CarApp
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,50 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lsvCars = new System.Windows.Forms.TextBox();
+            this.txtRegNr = new System.Windows.Forms.TextBox();
+            this.cbxForSale = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // label5
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label5";
             // 
-            // button2
+            // label6
             // 
-            this.button2.Location = new System.Drawing.Point(71, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 27);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(512, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "label6";
             // 
-            // button3
+            // label7
             // 
-            this.button3.Location = new System.Drawing.Point(167, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(80, 27);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(539, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "label7";
             // 
-            // Form1
+            // label8
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(689, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label8";
+            // 
+            // lsvCars
+            // 
+            this.lsvCars.Location = new System.Drawing.Point(528, 281);
+            this.lsvCars.Name = "lsvCars";
+            this.lsvCars.Size = new System.Drawing.Size(78, 26);
+            this.lsvCars.TabIndex = 4;
+            this.lsvCars.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txtRegNr
+            // 
+            this.txtRegNr.Location = new System.Drawing.Point(629, 281);
+            this.txtRegNr.Name = "txtRegNr";
+            this.txtRegNr.Size = new System.Drawing.Size(100, 26);
+            this.txtRegNr.TabIndex = 5;
+            this.txtRegNr.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // cbxForSale
+            // 
+            this.cbxForSale.Location = new System.Drawing.Point(692, 234);
+            this.cbxForSale.Name = "cbxForSale";
+            this.cbxForSale.Size = new System.Drawing.Size(105, 26);
+            this.cbxForSale.TabIndex = 6;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(516, 221);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(100, 26);
+            this.txtMake.TabIndex = 7;
+            this.txtMake.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(329, 190);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(113, 24);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(144, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 62);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "btnClear";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(185, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "btnAdd";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(205, 206);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "btnRemove";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(449, 60);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 84);
+            this.listBox2.TabIndex = 12;
+            // 
+            // form1
+            // 
+            this.ClientSize = new System.Drawing.Size(850, 358);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.cbxForSale);
+            this.Controls.Add(this.txtRegNr);
+            this.Controls.Add(this.lsvCars);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Name = "form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,6 +183,29 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox lsvCars;
+        private System.Windows.Forms.TextBox txtRegNr;
+        private System.Windows.Forms.TextBox cbxForSale;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- CarApp/Form1.cs
+++ CarApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,16 +11,60 @@ using System.Windows.Forms;
 
 namespace CarApp
 {
-    public partial class Form1 : Form
+    public partial class form1 : Form
     {
-        public Form1()
+        public form1()
         {
             InitializeComponent();
-            txtRegNr.focus();
+            txtRegNr.Focus();
         }
 
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtRegNr.Text) || string.IsNullOrEmpty(txtMake.Text)) { 
+
+                MessageBox.Show("Du måste fylla i alla rutor", "Felaktig inmatning");
+            }
+            else
+            {
+                ListViewItem item = CreateListViewItem(txtRegNr.Text, txtMake.Text, cbxForSale.Checked);
+                lsvCars.Items.Add(item);
+                ClearTextboxes();
+                btnClear.Enable = true;
+
+            }
+
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+           if (lsvCars.SelectedItems.count > 0)
+            {
+                var item = lsvCars.selectedItems[0];
+                lsvCars.Items.Remove(item);
+                MessageBox.Show("Bilen med registeringsnummer " + item.Text + "är borttagen", "Borttag av bil");
+
+            }
+            else
+            {
+                MessageBox.Show("Ingen bil var markerad att tas bort ", "Borttag av bil");
+            }
+            btnClear.Enabled = (lsvCars.Items.Count > 0);
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
         }
